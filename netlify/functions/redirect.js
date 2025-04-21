b4fb3a4b161902f86144f21f47644ff325a64baa
Hnzuonee <harvillajan@gmail.com>
2025-04-21 15:45:00 +0200

    Create redirect.js

---
+++ netlify/functions/redirect.js
@@ -0,0 +1,11 @@
+exports.handler = async (event) => {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
+
+  return {
+    statusCode: 200,
+    headers: { 'Content-Type': 'application/json', 'Cache-Control': 'no-cache' },
+    body: JSON.stringify({ url: 'https://onlyfans.com/terezaosladilova' })
+  };
+};
